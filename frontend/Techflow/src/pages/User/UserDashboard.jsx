aa47a6fdac0ada7ce22a6fe34d9b2b50c17be738
mariomujica99 <mariomujica99@gmail.com>
2025-07-29 16:25:15 -0500

    Routing setup for React.

--- frontend/Techflow/src/pages/User/UserDashboard.jsx
+++ frontend/Techflow/src/pages/User/UserDashboard.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 
-const Dashboard = () => {
+const UserDashboard = () => {
   return (
     <div>Dashboard</div>
   )
 }
 
-export default Dashboard
+export default UserDashboard
