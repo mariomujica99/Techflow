5972b3b7259532f448e662697fa2fdc3d408f6b7
mariomujica99 <mariomujica99@gmail.com>
2025-11-11 14:30:01 -0600

    Grid changes for Task Cards.

--- frontend/Techflow/src/pages/Admin/ManageTasks.jsx
+++ frontend/Techflow/src/pages/Admin/ManageTasks.jsx
@@ -219,7 +219,7 @@ const ManageTasks = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-2 md:mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-2 md:mt-4">
           {allTasks?.map((item, index) => (
             <TaskCard
               key={item._id}
